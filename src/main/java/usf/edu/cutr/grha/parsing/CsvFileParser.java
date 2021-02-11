52e1bc6be858c4c7f3d9041c6695f79075d5f9ca
BumbleFlash <sudarshan97@gmail.com>
2021-02-10 19:13:05 -0500

    chore: Add univocity library to read csv

---
+++ src/main/java/usf/edu/cutr/grha/parsing/CsvFileParser.java
@@ -0,0 +1,35 @@
+package usf.edu.cutr.grha.parsing;
+
+import com.univocity.parsers.annotations.FixedWidth;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
+import com.univocity.parsers.fixed.FixedWidthFields;
+import com.univocity.parsers.fixed.FixedWidthParser;
+import com.univocity.parsers.fixed.FixedWidthParserSettings;
+import com.univocity.parsers.fixed.FixedWidthRoutines;
+import usf.edu.cutr.grha.ProcessorMain;
+
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.List;
+
+public class CsvFileParser extends ProcessorMain {
+    public final String filePath;
+
+    public CsvFileParser(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void parseFile() {
+        CsvParserSettings csvParserSettings = new CsvParserSettings();
+        csvParserSettings.getFormat().setLineSeparator("\n");
+        CsvParser csvParser = new CsvParser(csvParserSettings);
+        Reader reader = getReader(filePath);
+        if (reader != null) {
+            List<String[]> allRows = csvParser.parseAll(getReader(filePath));
+            for (String[] rows : allRows) {
+                System.out.println(Arrays.toString(rows));
+            }
+        }
+    }
+}
