55ab33036def0d19c08aef81414189431c13d710
BumbleFlash <sudarshan97@gmail.com>
2021-04-21 21:23:56 -0400

    chore: improved planData function

--- src/main/java/edu/usf/cutr/grha/otp/OtpService.kt
+++ src/main/java/edu/usf/cutr/grha/otp/OtpService.kt
@@ -5,10 +5,11 @@ import edu.usf.cutr.grha.utils.GtfsUtils
 import edu.usf.cutr.otp.plan.api.PlanApi
 import edu.usf.cutr.otp.plan.model.Planner
 import edu.usf.cutr.otp.plan.model.RequestParameters
-import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.*
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resumeWithException
 
 class OtpService(
@@ -17,47 +18,52 @@ class OtpService(
 ) {
 
     fun call() {
-        makePlanFlow()
+        getPlanData()
     }
 
     private fun getPlanData() {
         var i = 0
         var start = 0L
-        var planFlow: Flow<Planner>
-            while (i < 42) {
-                if (isValidLocation(
-                        chicagoTncData[i].pickupCentroidLatitude,
-                        chicagoTncData[i].pickupCentroidLongitude,
-                        chicagoTncData[i].dropoffCentroidLatitude,
-                        chicagoTncData[i].dropoffCentroidLongitude
-                    )
-                ) {
-                    i++
-                    continue
-                }
-                val origin = GtfsUtils.latLong(
-                    chicagoTncData[i].pickupCentroidLatitude,
-                    chicagoTncData[i].pickupCentroidLongitude
-                )
-                val destination = GtfsUtils.latLong(
-                    chicagoTncData[i].dropoffCentroidLatitude,
-                    chicagoTncData[i].dropoffCentroidLongitude
-                )
-                val requestParameters = RequestParameters(fromPlace = origin, toPlace = destination)
-                start = System.currentTimeMillis()
-                val planApi = PlanApi(url, requestParameters)
-                planFlow = flow {
-                    emit(makePlanRequest(planApi, i))
-                }
-                i++
-                runBlocking {
-                    planFlow.collect {
-                        val time = System.currentTimeMillis() - start
-                        println(it)
-                        println("Time: $time ms")
+        runBlocking {
+            var planFlow: Flow<Planner> = flow {}
+            while (i < 41) {
+                GlobalScope.launch {
+                    if (!isValidLocation(
+                            chicagoTncData[i].pickupCentroidLatitude,
+                            chicagoTncData[i].pickupCentroidLongitude,
+                            chicagoTncData[i].dropoffCentroidLatitude,
+                            chicagoTncData[i].dropoffCentroidLongitude
+                        )
+                    ) {
+                        i++
+                    } else {
+                        val origin = GtfsUtils.latLong(
+                            chicagoTncData[i].pickupCentroidLatitude,
+                            chicagoTncData[i].pickupCentroidLongitude
+                        )
+                        val destination = GtfsUtils.latLong(
+                            chicagoTncData[i].dropoffCentroidLatitude,
+                            chicagoTncData[i].dropoffCentroidLongitude
+                        )
+                        val requestParameters = RequestParameters(fromPlace = origin, toPlace = destination)
+                        start = System.currentTimeMillis()
+                        val planApi = PlanApi(url, requestParameters)
+                        planFlow = flow {
+                            emit(makePlanRequest(planApi))
+                            println("Emitted Record: $i")
+                            i++
+                        }
                     }
                 }
+                planFlow.collect {
+                    val time = System.currentTimeMillis() - start
+                    println("Consumed Record: $i")
+                    println(it)
+                    println("Time: $time ms")
+                }
             }
+        }
+
 
     }
 
@@ -85,13 +91,13 @@ class OtpService(
 
                     val requestParameters = RequestParameters(fromPlace = origin, toPlace = destination)
                     val planApi = PlanApi(url, requestParameters)
-                    println(makePlanRequest(planApi, chicagoTncData.indexOf(it)))
+                    println(makePlanRequest(planApi))
                     println(chicagoTncData.indexOf(it))
                 }
         }
     }
 
-    private suspend fun makePlanRequest(planApi: PlanApi, i: Int): Planner = suspendCancellableCoroutine { cont ->
+    private suspend fun makePlanRequest(planApi: PlanApi): Planner = suspendCancellableCoroutine { cont ->
         planApi.getPlan({
             cont.resume(it, null)
         }, {
