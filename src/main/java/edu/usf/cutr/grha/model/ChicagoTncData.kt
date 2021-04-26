d03ea0f9a66749b19031a0db8a136161fb7234f5
BumbleFlash <sudarshan97@gmail.com>
2021-04-25 20:24:26 -0400

    refactor: added 3 versions of output parameters and changed list to String

--- src/main/java/edu/usf/cutr/grha/model/ChicagoTncData.kt
+++ src/main/java/edu/usf/cutr/grha/model/ChicagoTncData.kt
@@ -16,7 +16,6 @@
 package edu.usf.cutr.grha.model
 
 import com.univocity.parsers.annotations.Parsed
-import edu.usf.cutr.otp.plan.model.core.TraverseModes
 
 data class ChicagoTncData
 (
@@ -39,11 +38,28 @@ data class ChicagoTncData
     @Parsed val pickupCentroidLongitude: Double = 0.0,
     @Parsed val dropoffCentroidLatitude: Double = 0.0,
     @Parsed val dropoffCentroidLongitude: Double = 0.0,
-    @Parsed var totalTravelTime: Int? = 0,
-    @Parsed var walkTime: Int? = 0,
-    @Parsed var transitTime: Int? = 0,
-    @Parsed var totalDistance: Double? = 0.0,
-    @Parsed var altitudeChange: Double? = 0.0,
-    @Parsed var transfers: Int? = 0,
-    @Parsed var Modes: List<TraverseModes>? = null
+
+    @Parsed var totalTravelTime1: Int? = 0,
+    @Parsed var walkTime1: Int? = 0,
+    @Parsed var transitTime1: Int? = 0,
+    @Parsed var totalDistance1: Double? = 0.0,
+    @Parsed var altitudeChange1: Double? = 0.0,
+    @Parsed var transfers1: Int? = 0,
+    @Parsed var Modes1: String? = null,
+
+    @Parsed var totalTravelTime2: Int? = 0,
+    @Parsed var walkTime2: Int? = 0,
+    @Parsed var transitTime2: Int? = 0,
+    @Parsed var totalDistance2: Double? = 0.0,
+    @Parsed var altitudeChange2: Double? = 0.0,
+    @Parsed var transfers2: Int? = 0,
+    @Parsed var Modes2: String? = null,
+
+    @Parsed var totalTravelTime3: Int? = 0,
+    @Parsed var walkTime3: Int? = 0,
+    @Parsed var transitTime3: Int? = 0,
+    @Parsed var totalDistance3: Double? = 0.0,
+    @Parsed var altitudeChange3: Double? = 0.0,
+    @Parsed var transfers3: Int? = 0,
+    @Parsed var Modes3: String? = null
 )
